a1bdaa043530644f00f3a416d96020f809f24b7a
spazdrum <spazdrum@gmail.com>
2020-04-10 22:47:00 -0400

    postsPage completed

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -9,7 +9,7 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
-  const [likes, ] useState(props.posts.likes);
+  const [likes, ] = useState(props.posts.likes);
 
   return (
     <div className="post-border">
